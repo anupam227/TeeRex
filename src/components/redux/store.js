5eb476792b5ed1ac31133da66417f4e51dd02a02
AnupamWeb <anupam@appykan.com>
2022-06-20 11:26:45 +0530

    prod issue resolved

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -6,7 +6,10 @@ const store = createStore(
   allReducer,
   compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    typeof window.__REDUX_DEVTOOLS_EXTENSION__ === "undefined"
+      ? (a) => a
+      : window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
